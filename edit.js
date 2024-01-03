5fb585bf45242af96375a78d49a61385bac5d871
Ted <ted.gitonga@student.moringaschool.com>
2024-01-03 21:51:01 +0300

    Completed Assignment

---
+++ edit.js
@@ -0,0 +1,14 @@
+const marks = '59'
+
+let grade;
+if (marks >= 79){
+    grade = 'A';
+} else if (marks >= 60){
+    grade = 'B';
+} else if (marks >= 49){
+    grade = 'C';
+} else if (marks >= 40){
+    grade = 'D'
+} else {
+    grade = 'E'
+}
