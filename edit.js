ca1ba349dfd3aca26ea1b510536f88a8eaa5ca56
alinolenox <+>
2024-01-09 00:36:04 +0300

    completed assignment

--- edit.js
+++ edit.js
@@ -1,14 +1,24 @@
-const marks = '59'
 
-let grade;
-if (marks >= 79){
-    grade = 'A';
-} else if (marks >= 60){
-    grade = 'B';
-} else if (marks >= 49){
-    grade = 'C';
-} else if (marks >= 40){
-    grade = 'D'
-} else {
-    grade = 'E'
-}
+    function calculateGrade() {
+        
+        var marks = parseFloat(document.getElementById("marksInput").value);
+
+        if (isNaN(marks)) {
+            alert("Please enter a valid number.");
+            return;
+        }
+        var grade;
+        if (marks > 79) {
+            grade = 'A';
+        } else if (marks >= 60 && marks <= 79) {
+            grade = 'B';
+        } else if (marks >= 50 && marks <= 59) {
+            grade = 'C';
+        } else if (marks >= 40 && marks <= 49) {
+            grade = 'D';
+        } else {
+            grade = 'E';
+        }
+
+        document.getElementById("result").innerHTML = 'Grade: ' + grade;
+    }
